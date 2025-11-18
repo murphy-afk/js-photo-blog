d06cba2529dc684e0d9703204a3b146185006e85
murphy-afk <bardarenoemi@gmail.com>
2025-11-18 16:57:12 +0100

    feat: refactoring

--- js/script.js
+++ js/script.js
@@ -3,18 +3,24 @@
 const baseUrl = "https://lanciweb.github.io/demo/api/pictures/";
 const photosContainer = document.querySelector(".container");
 
+ const myCreateElement = (tagType, className) => {
+  const newElem = document.createElement(tagType);
+  newElem.classList.add(className);
+  return newElem
+ }
+
 const createCard = (photo) => {
   const { title, url, date } = photo;
-  const colElem = document.createElement('div');
-  colElem.classList.add("col");
-  colElem.innerHTML = `
-      <div class="card">
-        <div class="card-content">
+  const colElem = myCreateElement('div', "col");
+  const cardElem = myCreateElement('div', "card");
+  colElem.append(cardElem);
+  const cardContent = myCreateElement('div', "card-content");
+  cardElem.append(cardContent);
+  cardContent.append(img)
+  cardContent.innerHTML = `
           <img src="${url}" alt="${title}">
           <time datetime="datetime">${date}</time>
           <h2 class="card-title">${title}</h2>
-        </div>
-      </div>
         `;
   return colElem
 }
