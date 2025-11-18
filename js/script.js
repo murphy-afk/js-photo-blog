751907c5d616cc26d0716982484850a3cbf89dc7
murphy-afk <bardarenoemi@gmail.com>
2025-11-18 16:39:31 +0100

    feat: refactoring

--- js/script.js
+++ js/script.js
@@ -3,17 +3,11 @@
 const baseUrl = "https://lanciweb.github.io/demo/api/pictures/";
 const photosContainer = document.querySelector(".container");
 
-
-axios
-  .get("https://lanciweb.github.io/demo/api/pictures/")
-  .then((resp) => {
-    // console.log(resp.data);
-    const photoData = resp.data;
-    photoData.forEach((photo) => {
-      const { title, url, date } = photo;
-      const colElem = document.createElement('div');
-      colElem.classList.add("col");
-      colElem.innerHTML = `
+const createCard = (photo) => {
+  const { title, url, date } = photo;
+  const colElem = document.createElement('div');
+  colElem.classList.add("col");
+  colElem.innerHTML = `
       <div class="card">
         <div class="card-content">
           <img src="${url}" alt="${title}">
@@ -22,7 +16,16 @@ axios
         </div>
       </div>
         `;
-      photosContainer.append(colElem);
+  return colElem
+}
+
+axios
+  .get("https://lanciweb.github.io/demo/api/pictures/")
+  .then((resp) => {
+    const photoData = resp.data;
+    photoData.forEach((photo) => {
+      const photoCard = createCard(photo);
+      photosContainer.append(photoCard);
     })
   });
 
